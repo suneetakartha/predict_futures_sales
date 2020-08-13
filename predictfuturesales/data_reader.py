9ed3eec6fec9c0c44404f5b15872e48b541bc0b9
Suneeta Kartha <skartha@trumid.com>
2020-08-12 23:05:44 -0400

    adding test data parsing to data reader py

--- predictfuturesales/data_reader.py
+++ predictfuturesales/data_reader.py
@@ -6,5 +6,4 @@ class DataReader:
         self.item_categories = pandas.read_csv(os.path.join(directory, 'item_categories.csv'))
         self.items = pandas.read_csv(os.path.join(directory, 'items.csv'))
         self.sales_train = pandas.read_csv(os.path.join(directory, 'sales_train.csv'))
-            
-
+        self.test = pandas.read_csv(os.path.join(directory, 'test.csv'))
