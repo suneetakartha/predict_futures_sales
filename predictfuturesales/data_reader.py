385c2c017b828f957cde4facb02c7729368a9731
Rustam Aliyev <rustam@trumidtech.com>
2020-08-16 18:24:54 +0100

    Add linter and formatter

--- predictfuturesales/data_reader.py
+++ predictfuturesales/data_reader.py
@@ -1,10 +1,10 @@
 import pandas
 import os
 
+
 class DataReader:
     def __init__(self, directory):
-        self.item_categories = pandas.read_csv(os.path.join(directory, 'item_categories.csv'))
-        self.items = pandas.read_csv(os.path.join(directory, 'items.csv'))
-        self.sales_train = pandas.read_csv(os.path.join(directory, 'sales_train.csv'))
-            
-
+        categories_path = os.path.join(directory, "item_categories.csv")
+        self.item_categories = pandas.read_csv(categories_path)
+        self.items = pandas.read_csv(os.path.join(directory, "items.csv"))
+        self.sales_train = pandas.read_csv(os.path.join(directory, "sales_train.csv"))
